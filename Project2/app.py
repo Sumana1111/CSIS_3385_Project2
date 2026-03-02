666166dde0916578b97745dabf25bec9c71b6294
Sumana <sumana.ali@austincc.edu>
2026-03-02 10:03:08 -0600

    Update app.py by adding GET: Return all users, POST: Add a new use, PUT: Update user by ID, DELETE: Remove user by ID

--- Project2/app.py
+++ Project2/app.py
@@ -24,20 +24,59 @@ except FileNotFoundError:
 
 
 # GET: Return all users
+@app.route('/users', methods=['GET'])
+def get_users():
+   return jsonify(users), 200
+
 # cRud snippet goes here
 
 
+
 # POST: Add a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+   data = request.get_json()
+   new_user = {
+       'username': data.get('username'),
+       'email': data.get('email'),
+       'age': data.get('age'),
+       'password': data.get('password'),
+       'id': len(users) + 1
+   }
+   users.append(new_user)
+   return jsonify(new_user), 201
+
 # Crud snippet goes here
 
 
 
 # PUT: Update user by ID
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+   data = request.get_json()
+   for user in users:
+       if user['id'] == user_id:
+           user['username'] = data.get('username', user['username'])
+           user['email'] = data.get('email', user['email'])
+           user['age'] = data.get('age', user['age'])
+           user['password'] = data.get('password', user['password'])
+           return jsonify(user), 200
+   return jsonify({"error": "User not found"}), 404
+
 # crUd snippet goes here
 
 
 
 # DELETE: Remove user by ID
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+  global users
+  user = next((u for u in users if u['id'] == user_id), None)
+  if not user:
+    return jsonify({"error": "User not found"}), 404
+  users = [u for u in users if u['id'] != user_id]
+  return jsonify({"message": f"User {user_id} deleted"}), 200
+
 # cruD snippet goes here
 
 
